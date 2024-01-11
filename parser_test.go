d20aad3856c7005d71d16ab223e9701d6f09c430
mikejeffers <mike.s.jeffers@gmail.com>
2024-01-10 23:08:14 -0500

    more test cases and coverage

--- parser_test.go
+++ parser_test.go
@@ -7,15 +7,15 @@ import (
 func TestCommitParse(t *testing.T) {
 	commits := ParseCommits("commit hash\nDate:   2023-12-25T01:11:27-05:00\n2 files changed, 89 insertions(+)")
 	if len(commits) < 1 {
-		t.Fatalf("Failed to yield expected number of commits")
+		t.Errorf("Failed to yield expected number of commits")
 	} else if commits[0].date.IsZero() {
-		t.Fatalf("Failed to parse date %s", commits[0].date.String())
+		t.Errorf("Failed to parse date %s", commits[0].date.String())
 	} else if commits[0].filesChanged != 2 {
-		t.Fatalf("Incorrect filesChanged value %d", commits[0].filesChanged)
+		t.Errorf("Incorrect filesChanged value %d", commits[0].filesChanged)
 	} else if commits[0].insertions != 89 {
-		t.Fatalf("Incorrect insertions value %d", commits[0].insertions)
+		t.Errorf("Incorrect insertions value %d", commits[0].insertions)
 	} else if commits[0].deletions != 0 {
-		t.Fatalf("Incorrect deletions value %d", commits[0].deletions)
+		t.Errorf("Incorrect deletions value %d", commits[0].deletions)
 	}
 }
 
@@ -23,7 +23,18 @@ func TestCommitDateParse(t *testing.T) {
 	var commit Commit
 	commit.setDate("Date:   2023-12-25T01:11:27-05:00")
 	if commit.date.IsZero() {
-		t.Fatalf("Failed to parse date %s", commit.date.String())
+		t.Errorf("Failed to parse date %s", commit.date.String())
+	}
+}
+
+func TestCommitInvalidDateParse(t *testing.T) {
+	var commit Commit
+	commit.setDate("Date:   2023-1BLARG2-25T01:11:ohno27-05:00")
+	if !commit.date.IsZero() {
+		t.Errorf("Should have failed to parse date! %s", commit.date.String())
+	}
+	if commit.IsValid() {
+		t.Errorf("Should not be valid commit data")
 	}
 }
 
@@ -31,10 +42,34 @@ func TestCommitChangesParse(t *testing.T) {
 	var commit Commit
 	commit.setValues("2 files changed, 89 insertions(+)")
 	if commit.filesChanged != 2 {
-		t.Fatalf("Incorrect filesChanged value %d", commit.filesChanged)
+		t.Errorf("Incorrect filesChanged value %d", commit.filesChanged)
 	} else if commit.insertions != 89 {
-		t.Fatalf("Incorrect insertions value %d", commit.insertions)
+		t.Errorf("Incorrect insertions value %d", commit.insertions)
 	} else if commit.deletions != 0 {
-		t.Fatalf("Incorrect deletions value %d", commit.deletions)
+		t.Errorf("Incorrect deletions value %d", commit.deletions)
+	}
+}
+
+func TestCommitChangesParse2(t *testing.T) {
+	var commit Commit
+	commit.setValues("1 file changed, 1 deletion(-)")
+	if commit.filesChanged != 1 {
+		t.Errorf("Incorrect filesChanged value %d", commit.filesChanged)
+	} else if commit.insertions != 0 {
+		t.Errorf("Incorrect insertions value %d", commit.insertions)
+	} else if commit.deletions != 1 {
+		t.Errorf("Incorrect deletions value %d", commit.deletions)
+	}
+}
+
+func TestCommitChangesParse3(t *testing.T) {
+	var commit Commit
+	commit.setValues(" 1 file changed, 1 insertion(+), 1 deletion(-)")
+	if commit.filesChanged != 1 {
+		t.Errorf("Incorrect filesChanged value %d", commit.filesChanged)
+	} else if commit.insertions != 1 {
+		t.Errorf("Incorrect insertions value %d", commit.insertions)
+	} else if commit.deletions != 1 {
+		t.Errorf("Incorrect deletions value %d", commit.deletions)
 	}
 }
