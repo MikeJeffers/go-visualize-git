beee82ddf22aa6c094afed262ec2b4fde7b28530
mikejeffers <mike.s.jeffers@gmail.com>
2025-03-30 22:44:47 -0400

    legend, not bad

--- internal/drawing/draw.go
+++ internal/drawing/draw.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"log"
 	"math/rand/v2"
+	"strings"
 
 	c "gogitlog/internal/commits"
 	utils "gogitlog/internal/utils"
 
 	"github.com/fogleman/gg"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font/gofont/goregular"
 )
 
 var getTotalChanges = func(commit c.Commit) int {
@@ -80,6 +83,12 @@ func DrawCommits(buckets [][]c.Commit, width, height int) {
 
 // TODO
 func DrawCommitsByDirChanged(buckets [][]c.Commit, dirs []string, width, height int) {
+	font, err := truetype.Parse(goregular.TTF)
+	if err != nil {
+		log.Fatal(err)
+	}
+	face := truetype.NewFace(font, &truetype.Options{Size: 8})
+
 	stepW := float64(width) / float64(len(buckets))
 	maxChanged := 0
 	for _, bucket := range buckets {
@@ -93,8 +102,31 @@ func DrawCommitsByDirChanged(buckets [][]c.Commit, dirs []string, width, height 
 		colors = append(colors, rand.IntN(256*256*256))
 	}
 
-	dc := gg.NewContext(width, height)
-	// TODO draw legend showing color next to dir name
+	legendH := 120
+	dc := gg.NewContext(width, height+legendH)
+
+	stepXLegend := width / 10
+	stepYLegend := legendH / 4
+	xLegend := stepXLegend / 4
+	yLegend := height + stepYLegend/2
+	dc.SetFontFace(face)
+	for j, dir := range dirs {
+		if strings.ContainsAny(dir, ".=>") {
+			continue
+		}
+		dc.SetHexColor(fmt.Sprintf("#%06X", colors[j]))
+		xL, yL, wL, hL := float64(xLegend), float64(yLegend), float64(stepXLegend)*0.95, float64(stepYLegend)*0.95
+		dc.DrawRectangle(xL, yL, wL, hL)
+		dc.Fill()
+		dc.SetRGB(0, 0, 0)
+		dc.DrawStringAnchored(dir, xL+wL/2, yL+hL/2, 0.5, 0.5)
+		xLegend += stepXLegend
+		if xLegend+stepXLegend >= width {
+			xLegend = stepXLegend / 2
+			yLegend += stepYLegend
+		}
+	}
+
 	scaleChanges := float64(height) / float64(maxChanged)
 	for i, bucket := range buckets {
 		y := float64(height)
@@ -108,7 +140,7 @@ func DrawCommitsByDirChanged(buckets [][]c.Commit, dirs []string, width, height 
 			})
 			cHeight := float64(changesInDir) * scaleChanges
 			dc.SetHexColor(fmt.Sprintf("#%06X", colors[j]))
-			fmt.Println(fmt.Sprintf("#%06X", colors[j]))
+			// fmt.Println(fmt.Sprintf("#%06X", colors[j]))
 			dc.DrawRectangle(stepW*float64(i), y, stepW, -cHeight)
 			dc.Fill()
 			y -= cHeight
@@ -123,7 +155,7 @@ func DrawCommitsByDirChanged(buckets [][]c.Commit, dirs []string, width, height 
 		dc.Stroke()
 	}
 
-	err := dc.SavePNG("out.png")
+	err = dc.SavePNG("out.png")
 	if err != nil {
 		log.Fatalf("Failed to save output!")
 	}
